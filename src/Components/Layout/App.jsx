03a74f5347bc674bdfa86d7de9942d7499224a93
raju-ahammad <web.rajuahammad@gmail.com>
2020-09-14 21:54:12 +0600

    fixed header

--- src/Components/Layout/App.jsx
+++ src/Components/Layout/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Header from './Header/Header';
+import Header from '../Header/Header';
+import "./App.css";
 
 export class App extends Component {
     render() {
